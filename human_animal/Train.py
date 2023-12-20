bc62c9db60749c864cbcf95460070b1f8c0a0daf
Emene-Pesh <emeneche@gmail.com>
2023-12-20 16:11:40 +0100

    Code cleaning

--- human_animal/Train.py
+++ human_animal/Train.py
@@ -1,5 +1,6 @@
+import tensorflow as tf
 import keras
-from keras.layers import Input, Lambda, Dense, Flatten,Dropout,Conv2D,Rescaling,MaxPooling2D
+from keras.layers import Input, Lambda, Dense, Flatten,Dropout,Conv2D,Rescaling,MaxPooling2D, BatchNormalization
 from keras.models import Model
 from keras.applications.vgg19 import VGG19
 from keras.applications.vgg19 import preprocess_input
@@ -12,64 +13,60 @@ import pandas as pd
 import os
 import cv2
 import matplotlib.pyplot as plt
-# data_dir = "C:/Users/admin/Desktop/traintest"
-data_dir = "D:/DATASETS/Datasets_tests"
-batch_size = 32
+from keras.regularizers import l2
+from tensorflow.python.client import device_lib
+import datetime
+
+start_time = datetime.datetime.now()
+print("Time of start:", start_time)
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+#Directory of the dataset for humans and animals
+data_dir0 = "D:/DATASETS/Datasets_tests"
+data_dir1= "D:/DATASETS/Datasets_vals"
+#Directory of the dataset for animals
+data_dir= "D:/DATASETS/animals"
+data_dir2= "D:/DATASETS/val_animals"
+batch_size = 16
 img_height = 180
 img_width = 180
+import matplotlib.pyplot as plt
 
-train_path="C:/Users/admin/Desktop/traintest"
-test_dir="rps-final-dataset/test"
-val_dir="C:/Users/admin/Desktop/val_test"
-x_train=[]
-train_ds =keras.utils.image_dataset_from_directory(data_dir,
-  validation_split=0.2,
-  subset="training",
+val_ds=keras.utils.image_dataset_from_directory(data_dir1,
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
-
-val_ds=keras.utils.image_dataset_from_directory(data_dir,
-  validation_split=0.2,
-  subset="validation",
+train_ds =keras.utils.image_dataset_from_directory(data_dir0,
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
-
 class_names = train_ds.class_names
 print(class_names)
 
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-# plt.show()
 
 
 num_classes = len(class_names)
-#create model
+
 model = Sequential([
-  Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  Conv2D(16, 3, padding='same', activation='relu'),
-  MaxPooling2D(),
-  Conv2D(32, 3, padding='same', activation='relu'),
-  MaxPooling2D(),
+  Conv2D(32, (3, 3), activation='relu', input_shape=(180, 180, 3)),
+  BatchNormalization(),
+  MaxPooling2D(2, 2),
+  Dropout(0.2),
+  Conv2D(64, (3, 3), activation='relu'),
+  MaxPooling2D(2, 2),
   Flatten(),
-  Dense(128, activation='relu'),
-  Dense(num_classes)
+  Dense(128, activation='relu', kernel_regularizer=l2(0.001)),
+  Dropout(0.2),
+  Dense(num_classes, activation='softmax', kernel_regularizer=l2(0.001)) # Assuming it's a classification problem
 ])
-#Compile the model
+
+opt = tf.keras.optimizers.RMSprop()
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 #Training the model
-epochs = 2
+epochs = 15
 history = model.fit(
   train_ds,
   validation_data=val_ds,
@@ -79,10 +76,20 @@ history = model.fit(
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
+
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
+current_time = datetime.datetime.now()
+print("Current time:", current_time)
+
+time_difference = current_time - start_time
+minutes_difference = time_difference.total_seconds() / 60
+
+print("Time difference in minutes:", minutes_difference)
+
+
 
 plt.figure(figsize=(8, 8))
 plt.subplot(1, 2, 1)
@@ -97,7 +104,7 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
-model.save("./human_animal/model.keras")
+model.save("./human_animal/hora.keras")
 
 
 test_img_path= "C:/Users/admin/Desktop/AI_TEST/Lions.jpg"
